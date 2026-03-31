0e1d6f433c1dfbfeb25cc0df3d12329f7f66d6e9
divyanshchahar <divyanshchahar@outlook.com>
2026-03-31 21:45:54 +0530

    update: updated config file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-                port: '',
-                pathname: '**'
-            },
-        ]
-    }
-};
-export default nextConfig;
+        remotePatterns: [new URL('https://**')],
+    },
+}
