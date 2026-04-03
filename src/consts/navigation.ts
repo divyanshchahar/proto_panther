16e4c228441de15fc6e0e6dd069d815abbd8e412
divyanshchahar <divyanshchahar@outlook.com>
2026-04-03 16:18:39 +0530

    add: added new file

---
+++ src/consts/navigation.ts
@@ -0,0 +1,13 @@
+const navUrls = {
+    internal: {
+        home: "/",
+        contactUs: "/contactus",
+        aboutUs: "/aboutus",
+        services: "/services",
+    },
+    external: {
+        landingDiscoveryCall: "https://cal.com/divyanshchahar/landing",
+    }
+}
+
+export default navUrls;
