a2f2671bf32aef426c52e3da49e6389f76332430
divyanshchahar <divyanshchahar@outlook.com>
2026-04-04 15:49:54 +0530

    update: multiple updates removed `br` elements changed button verion from `cta` to `plain` added list of countries added `select`

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -7,6 +7,225 @@ import PhoneIconComponent from "../../../public/icons/PhoneIconComponent";
 import {useRouter} from "next/navigation";
 import navUrls from "@/consts/navigation";
 
+const listOfCountries = ["Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "British Virgin Islands",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cayman Islands",
+    "Central African Republic",
+    "Chad",
+    "Channel Islands",
+    "Chile",
+    "China",
+    "Colombia",
+    "Comoros",
+    "Democratic Republic of Congo",
+    "Republic of Congo",
+    "Costa Rica",
+    "Republic of Cote d'Ivoire",
+    "Croatia",
+    "Cuba",
+    "Curacao",
+    "Cyprus",
+    "Czechia",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Arab Republic of Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Faroe Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Polynesia",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guam",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hong Kong Special Administrative Region of the People's Republic of China ",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Islamic Republic of Iran",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Democratic Republic of Korea",
+    "Republic of Korea",
+    "Kosovo",
+    "Kuwait",
+    "Kyrgyz Republic",
+    "Lao PDR",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macao Special Administrative Region of the People's Republic of China",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Federal State of Micronesia",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "North Macedonia",
+    "Northern Mariana Islands",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Puerto Rico (US)",
+    "Qatatar",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Sint Maarten (Dutch part)",
+    "Slovak Republic",
+    "Slovenia",
+    "Solomon Islands",
+    "Federal Republic of Somalia",
+    "South Africa",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "St.Kitts and Nevis",
+    "St.Lucia",
+    "St.Martin(French part)",
+    "St.Vincent and the Grenadines",
+    "Sudan",
+    "Suriname",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkiye",
+    "Turkmenistan",
+    "Turks and Caicos Islands",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Bolivian Republic of Venezuella",
+    "VietNam",
+    "Virgin Islands (U.S.)",
+    "West Bank and Gaza",
+    "Republic of Yemen",
+    "Zambia",
+    "Zimbabwe"
+]
+
 
 export default function Page() {
     const router = useRouter();
@@ -41,28 +260,55 @@ export default function Page() {
 
             <div className={styles.BottomContainer}>
                 <form>
-                    <label className={`regularN`}>Name</label>
-                    <br/>
+                    <label className={`regularN`}>Full Name *</label>
+
                     <input type="text"/>
-                    <br/>
+
 
                     <label className={`regularN`}>Email</label>
-                    <br/>
+
                     <input type="email"/>
+
+
+                    <label className={`regularN`}>Country *</label>
+
+                    <select>
+                        {
+                            listOfCountries.map((country) => {
+                                return <option value={country} key={country}
+                                               selected={country === "India"}>{country}</option>
+                            })
+                        }
+                    </select>
+
+
+                    <label className={`regularN`}>Phone Number *</label>
+
+                    <input type="tel"/>
+
+
+                    <label className={`regularN`}>What do you need ?*</label>
+
+                    <select>
+                        <option value="Landing Page">Landing Page</option>
+
+                        <option value="Website">Website</option>
+
+                        <option value="MVP">MVP</option>
+                    </select>
+
                     <br/>
 
-                    <label className={`regularN`}>Message</label>
-                    <br/>
-                    <textarea/>
-                    <br/>
 
-                    <ButtonComponent version="cta" clickHandler={() => {
+                    <ButtonComponent version="plain" clickHandler={() => {
                         router.push(navUrls.external.landingDiscoveryCall)
                     }}>Schedule</ButtonComponent>
-
 
                 </form>
             </div>
         </div>
     )
 }
+
+// TODO
+// Improve International dialing code selection
